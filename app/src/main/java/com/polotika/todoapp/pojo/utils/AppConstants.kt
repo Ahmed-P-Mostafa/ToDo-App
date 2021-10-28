cf74485814fc557900c5b7a5c467bcb9b43f32c4
Ahmed Mostafa <ahmed.mostfa.ghorab@gmail.com>
2021-10-28 18:04:29 +0200

    Update AppConstants.kt

--- app/src/main/java/com/polotika/todoapp/pojo/utils/AppConstants.kt
+++ app/src/main/java/com/polotika/todoapp/pojo/utils/AppConstants.kt
@@ -5,7 +5,7 @@ object AppConstants {
 
     //Shared Preferences constants
     //TODO remove value before commit
-    const val preferencesName = "preferences_data_store"
+    const val preferencesName = "ADD YOUR PREFERENCES NAME"
     const val pSortingKey = "preferences_sorting_key"
     const val sortByDate = "1"
     const val sortByImportanceHigh = "2"
