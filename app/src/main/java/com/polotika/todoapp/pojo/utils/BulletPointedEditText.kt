eef99753a2829eff6be7d4d87f9e9d6d33a58a08
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-10-29 17:41:27 +0200

    add BulletPointedEditText.kt feature

---
+++ app/src/main/java/com/polotika/todoapp/pojo/utils/BulletPointedEditText.kt
@@ -0,0 +1,56 @@
+package com.polotika.todoapp.pojo.utils
+
+import com.google.android.material.textfield.TextInputEditText
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
+
+import android.R
+import android.content.Context
+
+import android.graphics.Typeface
+import android.util.AttributeSet
+
+class BulletPointedEditText(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+    TextInputEditText(context, attrs, defStyleAttr) {
+    var mContext: Context? = context
+    var mTypeface: Typeface? = null
+
+    constructor(context: Context, attrs: AttributeSet?) : this(
+        context,
+        attrs,
+        R.attr.editTextStyle
+    ) {
+    }
+
+    constructor(context: Context) : this(context, null, R.attr.editTextStyle) {}
+
+    override fun onTextChanged(
+        text: CharSequence,
+        start: Int,
+        lengthBefore: Int,
+        lengthAfter: Int
+    ) {
+        var text = text
+        if (lengthAfter > lengthBefore) {
+            if (text.toString().length == 1) {
+                text = "• $text"
+                setText(text)
+                setSelection(getText()?.length?:0)
+            }
+            if (text.toString().endsWith("\n")) {
+                text = text.toString().replace("\n", "\n• ")
+                text = text.toString().replace("• •", "•")
+                setText(text)
+                setSelection(getText()?.length?:0)
+            }
+        }
+        super.onTextChanged(text, start, lengthBefore, lengthAfter)
+    }
+
+    override fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
+        super.setPadding(left, top, right, bottom)
+    }
+
+
+
+
+}
